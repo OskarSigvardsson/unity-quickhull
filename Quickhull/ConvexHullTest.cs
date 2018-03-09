1006d4589d4b69ca60a28803d0d1d402a45d6ad2
Oskar Sigvardsson <oskar.sigvardsson@groplay.com>
2018-03-09 19:49:11 +0100

    Initial project commit

---
+++ Quickhull/ConvexHullTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GK {
+	public class ConvexHullTest : MonoBehaviour {
+
+		public Text DebugText;
+
+		IEnumerator Start() {
+			var calc = new ConvexHullCalculator();
+			var verts = new List<Vector3>();
+			var tris = new List<int>();
+			var normals = new List<Vector3>();
+			var points = new List<Vector3>();
+			var mf = GetComponent<MeshFilter>();
+			var mesh = new Mesh();
+
+			mf.sharedMesh = mesh;
+
+			while(true) {
+				points.Clear();
+
+				for (int i = 0; i < 500; i++) {
+					points.Add(new Vector3(
+							Random.value,
+							Random.value,
+							Random.value));
+
+				}
+
+				var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+				calc.GenerateHull(points, true, ref verts, ref tris, ref normals);
+
+				stopwatch.Stop();
+
+				DebugText.text = stopwatch.Elapsed.TotalMilliseconds.ToString("n2");
+
+				mesh.Clear();
+
+				mesh.SetVertices(verts);
+				mesh.SetTriangles(tris, 0);
+				mesh.SetNormals(normals);
+
+				yield return new WaitForSeconds(1.0f);
+
+			}
+		}
+	}
+}
