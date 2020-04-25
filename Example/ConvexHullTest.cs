5dee79cec4e1cd39e13a13891d10518c500a89f7
Oskar Sigvardsson <oskarsigvardsson@gmail.com>
2020-04-25 22:23:23 +0200

    Updated example

--- Example/ConvexHullTest.cs
+++ Example/ConvexHullTest.cs
@@ -6,37 +6,40 @@ using UnityEngine.UI;
 namespace GK {
 	public class ConvexHullTest : MonoBehaviour {
 
+		public GameObject RockPrefab;
+		
 		IEnumerator Start() {
 			var calc = new ConvexHullCalculator();
 			var verts = new List<Vector3>();
 			var tris = new List<int>();
 			var normals = new List<Vector3>();
 			var points = new List<Vector3>();
-			var mf = GetComponent<MeshFilter>();
-			var mesh = new Mesh();
-
-			mf.sharedMesh = mesh;
 
 			while(true) {
 				points.Clear();
 
-				for (int i = 0; i < 500; i++) {
-					points.Add(new Vector3(
-							Random.value,
-							Random.value,
-							Random.value));
-
+				for (int i = 0; i < 200; i++) {
+					points.Add(Random.insideUnitSphere);
 				}
 
 				calc.GenerateHull(points, true, ref verts, ref tris, ref normals);
 
-				mesh.Clear();
+				var rock = Instantiate(RockPrefab);
+
+				rock.transform.SetParent(transform, false);
+				rock.transform.localPosition = Vector3.zero;
+				rock.transform.localRotation = Quaternion.identity;
+				rock.transform.localScale = Vector3.one;
+				
+				var mesh = new Mesh();
 				mesh.SetVertices(verts);
 				mesh.SetTriangles(tris, 0);
 				mesh.SetNormals(normals);
 
-				yield return new WaitForSeconds(1.0f);
-
+				rock.GetComponent<MeshFilter>().sharedMesh = mesh;
+				rock.GetComponent<MeshCollider>().sharedMesh = mesh;
+				
+				yield return new WaitForSeconds(0.5f);
 			}
 		}
 	}
